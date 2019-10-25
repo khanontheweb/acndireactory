586f449b95306413ae631e13b7d8a0ec6379ed93
khanontheweb <mkhanongm@gmail.com>
2019-10-25 13:56:36 -0400

    Add create creative form

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-
+import CreativesList from "./components/creatives-list.component"
+import CreateCreative from "./components/create-creative.component"
+import EditCreative from "./components/edit-creative.component"
 
 
 function App() {
   return (
     <Router>
       <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
           <Link to="/" className="navbar-brand">
             ACN Direactory
           </Link>
-          <div className="collapse navbar-collapse">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
-              <li className="navbar-item">
+              <li className="nav-item">
                 <Link to="/" className="nav-link">Creatives</Link>
               </li>
-              <li className="navbar-item">
+              <li className="nav-item">
                 <Link to="/create" className="nav-link">Submit Yourself</Link>
               </li>
             </ul>
