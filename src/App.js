0adcc40248603ec3bcff47c3ff213ced888cc682
khanontheweb <mkhanongm@gmail.com>
2019-10-25 13:07:01 -0400

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className="container">
+      <h1>ACN Direactory</h1>
     </div>
   );
 }
