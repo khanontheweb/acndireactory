d1c2037cbec5b838cab366ce263e53b279b52ea5
khanontheweb <mkhanongm@gmail.com>
2019-10-25 14:55:41 -0400

    Added routes for creatives

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = 4000;
+const creativeRoutes = express.Router();
 
+app.use('/creatives');
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -18,3 +20,53 @@ connection.once('open', () => {
 app.listen(PORT, () => {
     console.log(`Server is running on Port:${PORT}`);
 })
+
+creativeRoutes.route('/').get((req, res) => {
+    Creative.find((err, creatives) => {
+        if(err)
+            console.log(err);
+        else
+            res.json(creatives);
+    });
+});
+
+creativeRoutes.route('/:id').get((req, res) => {
+    let id = req.params.id;
+    Creative.findById(id, (err, creative) => {
+        res.json(creative);
+    });
+});
+
+creativeRoutes.route('/add').post((req, res) => {
+    let creative = new Creative(req.body);
+
+    creative.save()
+    .then(creative => {
+        res.status(200).json({'creative': 'creative added successfully'});
+    })
+    .catch(err => {
+        res.status(400).send('adding new creative failed');
+    });
+});
+
+creativeRoutes.route('/update/:id').post((req, res) => {
+    Creative.findById(req.params.id, (err, creative) => {
+        if(!creative)
+            res.status(400).send('Cannot find creative');
+        else {
+            creative.name = req.body.name;
+            creative.email = req.body.email;
+            creative.instagram = req.body.instagram;
+            creative.facebook = req.body.facebook;
+            creative.mediums = req.body.mediums;
+
+            creative.save()
+            .then(creative => {
+                res.json('Creative updated');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            })
+        }
+    })
+});
