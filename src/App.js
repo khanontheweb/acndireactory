9123c3b91656b926b234a79b901c423d966664a6
khanontheweb <mkhanongm@gmail.com>
2019-10-25 13:10:48 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <h1>ACN Direactory</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <h1>ACN Direactory</h1>
+      </div>
+
+      <Route path="/" exact component={CreativesList}/>
+      <Route path="/edit/:id" exact component={EditCreative}/>
+      <Route path="/create" exact component={CreateCreative}/>
+    </Router>
   );
 }
 
